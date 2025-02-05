a48291879180f3b2e354ed6217d667101f5e16a1
saprativ99 <arijitchakma79@gmail.com>
2025-02-04 19:13:34 -0500

    added AuthBox

---
+++ client/components/AuthBox.tsx
@@ -0,0 +1,12 @@
+import React, {FC}from 'react'
+
+
+const AuthBox: FC = () => {
+    return (
+        <div className="auth-box">
+            AUthbox
+        </div>
+    )
+}
+
+export default AuthBox;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC,  } from 'react'
-import './App.css'
+import '../styles/App.css'
 
 const App: FC = () => {
   return (
